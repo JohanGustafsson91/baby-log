99db03aa7a4b20a3e4b9fe2dff22f3acc12c1413
Johan Gustafsson <johan.gustafsson91@gmail.com>
2023-12-20 13:41:41 +0100

    feature: hide url

--- services/client/src/components/App/App.tsx
+++ services/client/src/components/App/App.tsx
@@ -15,6 +15,8 @@ export const App = ({ Component, pageProps }: AppProps) => {
         <title>Baby Log</title>
         <meta name="description" content="Baby log app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
